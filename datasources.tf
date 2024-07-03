d55ff904c9983430a3904bb928a851cc01c34d77
Iryna Lyakhova <ippk0629@gmail.com>
2024-07-03 14:06:56 +0300

    Minor change

--- datasources.tf
+++ datasources.tf
@@ -2,7 +2,7 @@ data "azurerm_resource_group" "example" {
   name = "ilyakhova"
 }
 
-resource "azurerm_storage_account" "example" {
+resource "azurerm_storage_account" "storage_account_dafault" {
   name                     = "ilyakhova"
   resource_group_name      = data.azurerm_resource_group.example.name
   location                 = data.azurerm_resource_group.example.location
