05a1184c8e032f686f3c1c7da982c5ecffb255fe
Oleksii Yaroshenko <a.s.yaroshenko.zp@gmail.com>
2024-07-11 17:45:24 +0300

    needed changes

---
+++ datasources.tf
@@ -0,0 +1,3 @@
+data "azurerm_resource_group" "source_for_rg" {
+  name = "mate-terraform-task3-rg"
+}
--- main.tf
+++ main.tf
@@ -7,6 +7,4 @@ terraform {
   }
 }
 
-data "azurerm_resource_group" "source_for_rg" {
-  name = "mate-terraform-task3-rg"
-}
+
