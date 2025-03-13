7beaada6851e0cdcee4a0e045eb795d371aa09af
88victory88 <victory.otrok@gmail.com>
2025-03-13 13:42:44 +0800

    Solution

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "prefix" {
+  default = "tfvmex"
+}
+
+variable "location" {
+  default = "West Europe"
+}
+
+variable "admin_username" {
+  default = "testadmin"
+}
+
+variable "admin_password" {
+  default = "Password1234!"
+}
