25a481780ce50a5ef1d4af77bf63d748234cc33b
Marian Kostiuk <131563162+kostiukmkalne@users.noreply.github.com>
2025-04-10 08:42:59 -0500

    Solution trm

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "prefix" {
+  default = "tfvmex"
+}
+
+variable "computer_name" {
+  default = "hostname"
+}
+
+variable "admin_username" {
+  default = "testadmin"
+}
+
+variable "admin_password" {
+  default = "Password1234!"
+}
+
+variable "environment" {
+  default = "staging"
+}
