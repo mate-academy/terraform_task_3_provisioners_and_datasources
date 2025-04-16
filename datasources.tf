ffa2532467e3b5d5d78efc6dca7890a3681b791d
Beliar24 <blackdid7@gmail.com>
2025-04-16 21:11:46 +0300

    fix

--- datasources.tf
+++ datasources.tf
@@ -1,53 +1,39 @@
 data "azurerm_resource_group" "example" {
-  name     = "${var.prefix}-resources"
+  name = "${var.prefix}-resources"
 }
 
-resource "azurerm_virtual_network" "main" {
+data "azurerm_virtual_network" "main" {
   name                = "${var.prefix}-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = data.azurerm_resource_group.example.location
   resource_group_name = data.azurerm_resource_group.example.name
 }
 
-resource "azurerm_subnet" "internal" {
+data "azurerm_subnet" "internal" {
   name                 = "internal"
+  virtual_network_name = data.azurerm_virtual_network.main.name
   resource_group_name  = data.azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_network_interface" "main" {
+data "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic"
-  location            = data.azurerm_resource_group.example.location
   resource_group_name = data.azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "testconfiguration1"
-    subnet_id                     = azurerm_subnet.internal.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.example.id
-  }
 }
 
-resource "azurerm_public_ip" "example" {
- name                = "${var.prefix}-public-ip"
- location            = data.azurerm_resource_group.example.location
- resource_group_name = data.azurerm_resource_group.example.name
- allocation_method   = "Static"
+data "azurerm_public_ip" "example" {
+  name                = "${var.prefix}-public-ip"
+  resource_group_name = data.azurerm_resource_group.example.name
+}
+
+data "azurerm_virtual_machine" "main" {
+  name                = "${var.prefix}-vm"
+  resource_group_name = data.azurerm_resource_group.example.name
 }
 
 resource "azurerm_virtual_machine" "main" {
   name                  = "${var.prefix}-vm"
   location              = data.azurerm_resource_group.example.location
   resource_group_name   = data.azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.main.id]
+  network_interface_ids = [data.azurerm_network_interface.main.id]
   vm_size               = "Standard_DS1_v2"
-
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
-
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
 
   storage_image_reference {
     publisher = "Canonical"
@@ -78,19 +64,19 @@ resource "azurerm_virtual_machine" "main" {
     destination = "/tmp/default.html"
 
     connection {
-      type = "ssh"
-      user = "testadmin"
+      type     = "ssh"
+      user     = "testadmin"
       password = "Password1234!"
-      host = azurerm_public_ip.example.ip_address
+      host     = data.azurerm_public_ip.example.ip_address
     }
   }
 
   provisioner "remote-exec" {
     connection {
-      type = "ssh"
-      user = "testadmin"
+      type     = "ssh"
+      user     = "testadmin"
       password = "Password1234!"
-      host = azurerm_public_ip.example.ip_address
+      host     = data.azurerm_public_ip.example.ip_address
     }
 
     inline = [
