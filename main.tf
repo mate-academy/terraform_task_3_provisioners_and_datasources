c7ba607242608c48b6b1efcf54bccccf5b809afb
Yevgene-DP <ebalalay@gmail.com>
2025-09-18 23:34:17 +0300

    Creating an Azure Virtual Machine with Nginx web server

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.105.0"
     }
   }
@@ -15,11 +15,23 @@ variable "prefix" {
   default = "tfvmex"
 }
 
+variable "admin_username" {
+  default   = "testadmin"
+  sensitive = true
+}
+
+variable "admin_password" {
+  default   = "Password1234!"
+  sensitive = true
+}
+
+# Створення групи ресурсів
 resource "azurerm_resource_group" "example" {
   name     = "${var.prefix}-resources"
   location = "West Europe"
 }
 
+# Створення віртуальної мережі
 resource "azurerm_virtual_network" "main" {
   name                = "${var.prefix}-network"
   address_space       = ["10.0.0.0/16"]
@@ -27,6 +39,7 @@ resource "azurerm_virtual_network" "main" {
   resource_group_name = azurerm_resource_group.example.name
 }
 
+# Створення підмережі
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.example.name
@@ -34,6 +47,15 @@ resource "azurerm_subnet" "internal" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+# Створення публічного IP для доступу до Nginx
+resource "azurerm_public_ip" "main" {
+  name                = "${var.prefix}-publicip"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+# Створення мережевого інтерфейсу
 resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic"
   location            = azurerm_resource_group.example.location
@@ -43,21 +65,17 @@ resource "azurerm_network_interface" "main" {
     name                          = "testconfiguration1"
     subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.main.id
   }
 }
 
+# Створення віртуальної машини
 resource "azurerm_virtual_machine" "main" {
   name                  = "${var.prefix}-vm"
   location              = azurerm_resource_group.example.location
   resource_group_name   = azurerm_resource_group.example.name
   network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_DS1_v2"
-
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
-
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
+  vm_size               = "Standard_B1s"
 
   storage_image_reference {
     publisher = "Canonical"
@@ -65,21 +83,81 @@ resource "azurerm_virtual_machine" "main" {
     sku       = "22_04-lts"
     version   = "latest"
   }
+
   storage_os_disk {
     name              = "myosdisk1"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
+
   os_profile {
     computer_name  = "hostname"
-    admin_username = "testadmin"
-    admin_password = "Password1234!"
+    admin_username = var.admin_username
+    admin_password = var.admin_password
   }
+
   os_profile_linux_config {
     disable_password_authentication = false
   }
+
   tags = {
     environment = "staging"
   }
+
+  # Remote-exec provisioner для встановлення Nginx
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install -y nginx",
+      "sudo systemctl start nginx",
+      "sudo systemctl enable nginx"
+    ]
+
+    connection {
+      type     = "ssh"
+      user     = var.admin_username
+      password = var.admin_password
+      host     = azurerm_public_ip.main.ip_address
+    }
+  }
+
+  # File provisioner для завантаження сторінки
+  provisioner "file" {
+    source      = "index.html"
+    destination = "/tmp/index.html"
+
+    connection {
+      type     = "ssh"
+      user     = var.admin_username
+      password = var.admin_password
+      host     = azurerm_public_ip.main.ip_address
+    }
+  }
+
+  # Переміщення файлу та перезапуск Nginx
+  provisioner "remote-exec" {
+    inline = [
+      "sudo mv /tmp/index.html /var/www/html/index.html",
+      "sudo systemctl restart nginx"
+    ]
+
+    connection {
+      type     = "ssh"
+      user     = var.admin_username
+      password = var.admin_password
+      host     = azurerm_public_ip.main.ip_address
+    }
+  }
+}
+
+# Outputs для відображення публічного IP та URL
+output "public_ip" {
+  description = "Public IP address of the VM"
+  value       = azurerm_public_ip.main.ip_address
+}
+
+output "nginx_url" {
+  description = "URL to access Nginx"
+  value       = "http://${azurerm_public_ip.main.ip_address}"
 }
