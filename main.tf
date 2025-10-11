522da76d7292e27cdb74a6b7ce9856f3efe9a6be
Dmytro <demon9709>
2025-10-12 02:22:16 +0300

    Sol

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "azurerm_public_ip" "example" {
- name                = "${var.prefix}-public-ip"
- location            = data.azurerm_resource_group.rg.location
- resource_group_name = data.azurerm_resource_group.rg.name
- allocation_method   = "Static"
+  name                = "${var.prefix}-public-ip"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  allocation_method   = "Static"
 }
 
 
