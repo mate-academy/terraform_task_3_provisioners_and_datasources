cb88d46381d9b427fc3942c49279ec3c567bd534
Dmytro <demon9709>
2025-10-12 02:19:16 +0300

    AddSomeChanges

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "resource_group_name" {
-  type = string
+  type        = string
   description = "deploy-nginx"
 }
 
 variable "vm_name" {
-  type = string
+  type        = string
   description = "tfmex"
 }
