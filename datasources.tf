b74d1a9303df8f123855375767e9a292961686f6
salabam <pavelgrom.chaban@gmail.com>
2026-02-14 00:53:17 +0200

    Solution

---
+++ datasources.tf
@@ -0,0 +1,29 @@
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+}
+
+data "azurerm_virtual_network" "vnet" {
+  name                = var.virtual_network_name
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+data "azurerm_subnet" "subnet" {
+  name                 = var.subnet_name
+  virtual_network_name = data.azurerm_virtual_network.vnet.name
+  resource_group_name  = data.azurerm_resource_group.rg.name
+}
+
+data "azurerm_network_interface" "nic" {
+  name                = var.network_interface_name
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+data "azurerm_public_ip" "pip" {
+  name                = var.public_ip_name
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+data "azurerm_virtual_machine" "vm" {
+  name                = var.vm_name
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
