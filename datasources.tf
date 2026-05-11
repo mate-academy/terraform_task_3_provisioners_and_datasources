dfe5e3e08409422e6d21f733d21ada0def1141e0
bohn13 <bod3171@gmail.com>
2026-05-11 19:57:27 +0200

    updated again

--- datasources.tf
+++ datasources.tf
@@ -2,6 +2,17 @@ data "azurerm_resource_group" "example" {
   name = "my_resource_group"
 }
 
+data "azurerm_virtual_network" "example" {
+  name                = "${var.prefix}-network"
+  resource_group_name = data.azurerm_resource_group.example.name
+}
+
+data "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = data.azurerm_resource_group.example.name
+  virtual_network_name = data.azurerm_virtual_network.example.name
+}
+
 data "azurerm_public_ip" "example" {
   name                = "${var.prefix}-publicip"
   resource_group_name = data.azurerm_resource_group.example.name
@@ -15,15 +26,4 @@ data "azurerm_network_interface" "example" {
 data "azurerm_virtual_machine" "example" {
   name                = "${var.prefix}-vm"
   resource_group_name = data.azurerm_resource_group.example.name
-}
-
-data "azurerm_virtual_network" "example" {
-  name                = "${var.prefix}-network"
-  resource_group_name = data.azurerm_resource_group.example.name
-}
-
-data "azurerm_subnet" "example" {
-  name                 = "internal"
-  resource_group_name  = data.azurerm_resource_group.example.name
-  virtual_network_name = data.azurerm_virtual_network.example.name
 }
