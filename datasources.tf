29328913c7a268435fcff34c9114ec35d3f392df
Andrii Shukalo <shukaload@gmail.com>
2026-07-10 18:27:12 +0300

    Fixes

--- datasources.tf
+++ datasources.tf
@@ -2,11 +2,6 @@ data "azurerm_resource_group" "example" {
   name = "GroupForT3"
 }
 
-data "azurerm_virtual_machine" "example" {
-  name                = "${var.prefix}-vm"
-  resource_group_name = data.azurerm_resource_group.example.name
-}
-
 data "azurerm_virtual_network" "example" {
   name                = "${var.prefix}-network"
   resource_group_name = data.azurerm_resource_group.example.name
@@ -21,4 +16,9 @@ data "azurerm_subnet" "internal" {
   name                 = "internal"
   virtual_network_name = data.azurerm_virtual_network.example.name
   resource_group_name  = data.azurerm_resource_group.example.name
+}
+
+data "azurerm_public_ip" "example" {
+  name                = "${var.prefix}-public-ip"
+  resource_group_name = data.azurerm_resource_group.example.name
 }
