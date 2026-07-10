ce1fdf15a676acbea83290a575fb1224dae750a9
Andrii Shukalo <shukaload@gmail.com>
2026-07-10 18:11:47 +0300

    Fixes

--- datasources.tf
+++ datasources.tf
@@ -1,3 +1,24 @@
 data "azurerm_resource_group" "example" {
   name = "GroupForT3"
 }
+
+data "azurerm_virtual_machine" "example" {
+  name                = "${var.prefix}-vm"
+  resource_group_name = data.azurerm_resource_group.example.name
+}
+
+data "azurerm_virtual_network" "example" {
+  name                = "${var.prefix}-network"
+  resource_group_name = data.azurerm_resource_group.example.name
+}
+
+data "azurerm_network_interface" "example" {
+  name                = "${var.prefix}-nic"
+  resource_group_name = data.azurerm_resource_group.example.name
+}
+
+data "azurerm_subnet" "internal" {
+  name                 = "internal"
+  virtual_network_name = data.azurerm_virtual_network.example.name
+  resource_group_name  = data.azurerm_resource_group.example.name
+}
--- main.tf
+++ main.tf
@@ -1,43 +1,3 @@
-resource "azurerm_resource_group" "example" {
-  name     = "${var.prefix}-resources"
-  location = "Sweden Central"
-}
-
-resource "azurerm_virtual_network" "main" {
-  name                = "${var.prefix}-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = data.azurerm_resource_group.example.location
-  resource_group_name = data.azurerm_resource_group.example.name
-}
-
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  resource_group_name  = data.azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-resource "azurerm_public_ip" "main" {
-  name                = "${var.prefix}-public-ip"
-  location            = data.azurerm_resource_group.example.location
-  resource_group_name = data.azurerm_resource_group.example.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
-  location            = data.azurerm_resource_group.example.location
-  resource_group_name = data.azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "testconfiguration1"
-    subnet_id                     = azurerm_subnet.internal.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.main.id
-  }
-}
-
 resource "azurerm_network_security_group" "main" {
   name                = "acceptancegroup"
   location            = data.azurerm_resource_group.example.location
@@ -67,46 +27,15 @@ resource "azurerm_network_security_group" "main" {
 }
 
 resource "azurerm_network_interface_security_group_association" "main" {
-  network_interface_id      = azurerm_network_interface.main.id
+  network_interface_id      = data.azurerm_network_interface.example.id
   network_security_group_id = azurerm_network_security_group.main.id
 }
 
-resource "azurerm_virtual_machine" "main" {
-  name                  = "${var.prefix}-vm"
-  location              = data.azurerm_resource_group.example.location
-  resource_group_name   = data.azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_D2s_v3"
+resource "null_resource" "configure_nginx" {
+  triggers = {
+    vm_id = data.azurerm_virtual_machine.example.id
+  }
 
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
-
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-  storage_os_disk {
-    name              = "myosdisk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = "hostname"
-    admin_username = "testadmin"
-    admin_password = "Password1234!"
-  }
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-  tags = {
-    environment = "staging"
-  }
   provisioner "file" {
     source      = "./index.html"
     destination = "/home/testadmin/index.html"
@@ -115,7 +44,7 @@ resource "azurerm_virtual_machine" "main" {
       type     = "ssh"
       user     = "testadmin"
       password = "Password1234!"
-      host     = azurerm_public_ip.main.ip_address
+      host     = data.azurerm_virtual_machine.example.public_ip_address
     }
   }
   provisioner "remote-exec" {
@@ -130,9 +59,7 @@ resource "azurerm_virtual_machine" "main" {
       type     = "ssh"
       user     = "testadmin"
       password = "Password1234!"
-      host     = azurerm_public_ip.main.ip_address
+      host     = data.azurerm_virtual_machine.example.public_ip_address
     }
   }
-
-
 }
