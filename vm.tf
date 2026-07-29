cad731ff768084f10ef52bbd5c29d1749dc65691
TopalRuslan <baranotik15@gmail.com>
2026-07-29 13:59:46 +0300

    Add provisioner

--- vm.tf
+++ vm.tf
@@ -28,4 +28,27 @@ resource "azurerm_virtual_machine" "main" {
   tags = {
     environment = "staging"
   }
+
+  connection {
+    type     = "ssh"
+    host     = azurerm_public_ip.main.ip_address
+    port     = 22
+    user     = "testadmin"
+    password = "Password1234!"
+  }
+
+  provisioner "file" {
+    source      = "index.html"
+    destination = "/tmp/index.html"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo apt install -y nginx",
+      "sudo mv /tmp/index.html /var/www/html/index.html",
+      "sudo systemctl restart nginx"
+    ]
+  }
+
 }
