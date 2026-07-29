c2094bb594692dbffc0b29df8baa5412ecfd8061
TopalRuslan <baranotik15@gmail.com>
2026-07-29 13:39:47 +0300

    Add resource group datasource for terraform task 3

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "resource_group_name" {
+  description = "Name of the manually created resource group"
+  type        = string
+  default     = "rg-terraform-task-3"
+}
+
+
+variable "prefix" {
+  default = "tfvmex"
+}
