3f82a717906965361292940023c48d5e381743df
Anastasiia <anastasiia.chikrizova@dashdevs.com>
2026-08-04 14:16:23 +0300

    fix ai comments

--- main.tf
+++ main.tf
@@ -4,44 +4,16 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "3.105.0"
     }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
   }
 }
 
-resource "azurerm_virtual_machine" "main" {
-  name                  = "${var.prefix}-vm"
-  location              = data.azurerm_resource_group.example.location
-  resource_group_name   = data.azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_DS1_v2"
-
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
-
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-  storage_os_disk {
-    name              = "myosdisk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = "hostname"
-    admin_username = "testadmin"
-    admin_password = "Password1234!"
-  }
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-  tags = {
-    environment = "staging"
+resource "null_resource" "provision_vm" {
+  triggers = {
+    vm_id = data.azurerm_virtual_machine.main.id
   }
 
   provisioner "file" {
@@ -52,17 +24,18 @@ resource "azurerm_virtual_machine" "main" {
       type     = "ssh"
       user     = "testadmin"
       password = "Password1234!"
-      host     = azurerm_public_ip.example.ip_address
+      host     = data.azurerm_public_ip.example.ip_address
+      timeout  = "30m"
     }
   }
-
 
   provisioner "remote-exec" {
     connection {
       type     = "ssh"
       user     = "testadmin"
       password = "Password1234!"
-      host     = azurerm_public_ip.example.ip_address
+      host     = data.azurerm_public_ip.example.ip_address
+      timeout  = "30m"
     }
 
     inline = [
