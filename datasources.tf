0824827393a5ee511087433d62423a53ddc5699d
nikol <77391893+nikol-reukr@users.noreply.github.com>
2026-09-06 19:49:23 +0300

    Add complete Terraform infrastructure and Nginx provisioners

---
+++ datasources.tf
@@ -0,0 +1,24 @@
+data "azurerm_resource_group" "example" {
+  name = var.resource_group_name
+}
+
+data "azurerm_virtual_network" "vnet" {
+  name                = azurerm_virtual_network.main.name
+  resource_group_name = data.azurerm_resource_group.example.name
+}
+
+data "azurerm_subnet" "subnet" {
+  name                 = azurerm_subnet.internal.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  resource_group_name  = data.azurerm_resource_group.example.name
+}
+
+data "azurerm_network_interface" "nic" {
+  name                = azurerm_network_interface.main.name
+  resource_group_name = data.azurerm_resource_group.example.name
+}
+
+data "azurerm_virtual_machine" "vm" {
+  name                = azurerm_virtual_machine.main.name
+  resource_group_name = data.azurerm_resource_group.example.name
+}
