23736acb62b48e9d5e0820c0a35ca4586bc9b71c
nikol <77391893+nikol-reukr@users.noreply.github.com>
2026-09-06 19:57:04 +0300

    Add depends on meta-argument to datasources

--- datasources.tf
+++ datasources.tf
@@ -5,20 +5,28 @@ data "azurerm_resource_group" "example" {
 data "azurerm_virtual_network" "vnet" {
   name                = azurerm_virtual_network.main.name
   resource_group_name = data.azurerm_resource_group.example.name
+
+  depends_on = [azurerm_virtual_network.main]
 }
 
 data "azurerm_subnet" "subnet" {
   name                 = azurerm_subnet.internal.name
   virtual_network_name = azurerm_virtual_network.main.name
   resource_group_name  = data.azurerm_resource_group.example.name
+
+  depends_on = [azurerm_subnet.internal]
 }
 
 data "azurerm_network_interface" "nic" {
   name                = azurerm_network_interface.main.name
   resource_group_name = data.azurerm_resource_group.example.name
+
+  depends_on = [azurerm_network_interface.main]
 }
 
 data "azurerm_virtual_machine" "vm" {
   name                = azurerm_virtual_machine.main.name
   resource_group_name = data.azurerm_resource_group.example.name
+
+  depends_on = [azurerm_virtual_machine.main]
 }
